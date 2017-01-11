b30e6fa1111541d1c77e6ecc31d94b1c2b89dfac
William Belle <william.belle@gmail.com>
2017-01-11 10:12:06 +0100

    implement isHONcodeCertified function

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var HONcodeCertificationList = require('honcode-certification-list');
+var HONcodeUtils = require('honcode-certification-utils');
+
+var Promise = require('bluebird');
+
+exports.isHONcodeCertified = function(url) {
+
+  // Get HONcode list
+  return HONcodeCertificationList.getHONcodeCertificationList().then(
+    function(hash) {
+      // Prepare url
+      url = HONcodeUtils.formatUrl(url);
+      var listUrls = HONcodeUtils.buildUrlToCheck(url);
+
+      return HONcodeCertificationList.isInList(hash, listUrls);
+    }).catch(function(reason) {
+      console.log(reason);
+    }
+  );
+};
--- test/test.js
+++ test/test.js
@@ -1,1 +1,26 @@
 'use strict';
+
+var should = require('chai').should();
+var isHONcodeCertified = require('../src/lib/index.js');
+
+describe('is HONcode Certified', function() {
+
+  it('should return true for webmd.com', function(done) {
+    isHONcodeCertified.isHONcodeCertified('http://www.webmd.com/').then(
+      function(isCertified) {
+        isCertified.should.equal(true);
+        done();
+      }
+    );
+  });
+
+  it('should return false for playboy.com', function(done) {
+    isHONcodeCertified.isHONcodeCertified('http://www.playboy.com/').then(
+      function(isCertified) {
+        isCertified.should.equal(false);
+        done();
+      }
+    );
+  });
+
+});
